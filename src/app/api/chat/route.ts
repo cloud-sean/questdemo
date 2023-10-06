f13b7a3e537c1b00d30ce2465f7ebe0ae530cf7f
Sean Hamill <seanhamill@Seans-MacBook-Pro.local>
2023-10-06 17:16:29 -0400

    clean up

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,5 +2,6 @@ import { chatAPIEntry } from "@/features/chat/chat-services/chat-api-entry";
 
 export async function POST(req: Request) {
   const body = await req.json();
+  console.log(body);
   return await chatAPIEntry(body);
 }
